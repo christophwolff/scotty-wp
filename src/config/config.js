00f2e914e731cd43df63b34494e3b470bec79f57
Christoph Wolff <cw@christophwolff.de>
2016-08-22 14:21:11 +0200

    Add Pages API Endpoint

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,4 @@
 export const ROOT_URL = 'http://scotty.dev/';
 export const ROOT_API  = ROOT_URL + 'wp-json/'
 export const POSTS_URL = ROOT_API + 'wp/v2/posts/';
+export const PAGES_URL = ROOT_API + 'wp/v2/pages/';
